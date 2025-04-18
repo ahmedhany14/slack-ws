1414752c5f9eac58a0bdd7a1ac536613a0fd07a9
ahmed_hany <ah7608867@gmail.com>
2025-04-18 23:47:07 +0200

    apply new response style to friend controller

--- apps/slack/src/friends/friends.controller.ts
+++ apps/slack/src/friends/friends.controller.ts
@@ -28,24 +28,53 @@ export class FriendsController {
     ) {}
 
     /**
-     * Function to get all friends of a user
-     * @param user_id
-     * @returns List of friends
+     * Retrieves all friends for the specified user.
+     *
+     * @param {number} user_id - The ID of the user whose friends are to be retrieved.
+     * @return {Promise<{ response: { friends: { id: number; name: string; }[]; }; }>} - A promise that resolves to an object containing the list of friends.
      */
     @Get('all-friends')
-    async getAllFriends(@ExtractUserData('id') user_id: number) {
+    async getAllFriends(@ExtractUserData('id') user_id: number): Promise<{
+        response: {
+            friends: {
+                id: number;
+                name: string;
+            }[];
+        };
+    }> {
         this.logger.log('getAllFriends of user_id = ', user_id);
 
-        return this.friendsService.getMyFriends(user_id);
+        const friends = await this.friendsService.getMyFriends(user_id);
+
+        return {
+            response: {
+                friends,
+            },
+        };
     }
 
     /**
-     * Function to get all friends invitations of a user (sent to the user)
-     * @param user_id
-     * @return List of friends invitations
+     * Retrieves all pending friend invitations for a given user.
+     *
+     * @param {number} user_id - The ID of the user whose pending friend invitations are to be retrieved.
+     * @return {Promise<Array<Object>>} A promise that resolves to a list of friend invitations. Each invitation includes:
+     *  - id: The ID of the invitation.
+     *  - sender: An object containing the sender's ID and username.
+     *  - request_status: The status of the friend invitation request.
      */
     @Get('all-friends-invitations')
-    async getAllFriendsInvitations(@ExtractUserData('id') user_id: number) {
+    async getAllFriendsInvitations(@ExtractUserData('id') user_id: number): Promise<{
+        response: {
+            invitations: {
+                id: number;
+                sender: {
+                    id: number;
+                    username: string;
+                };
+                request_status: RequestStatus;
+            }[];
+        };
+    }> {
         this.logger.log('getAllFriendsInvitations of user_id = ', user_id);
 
         const friends_invitations = await this.friendsService.find({
@@ -53,7 +82,7 @@ export class FriendsController {
             request_status: RequestStatus.PENDING,
         } as FriendsInvitations);
 
-        return friends_invitations.map((invitation) => ({
+        const invitations = friends_invitations.map((invitation) => ({
             id: invitation.id,
             sender: {
                 id: invitation.sender.id,
@@ -61,38 +90,61 @@ export class FriendsController {
             },
             request_status: invitation.request_status,
         }));
+
+        return {
+            response: { invitations },
+        };
     }
 
     /**
-     * Function to get all friend requests sent to a user
-     * @param user_id
-     * @return List of friend requests
+     * Fetches the list of pending friend requests for the given user.
+     *
+     * @param {number} user_id - The unique identifier of the user whose pending friend requests are being retrieved.
+     * @return {Promise<{ response: { pendingRequests: FriendsInvitations[]; }; }>} - A promise that resolves to an object containing the list of pending friend requests.
      */
     @Get('pending-friend-requests')
-    async getPendingFriendRequests(@ExtractUserData('id') user_id: number) {
+    async getPendingFriendRequests(@ExtractUserData('id') user_id: number): Promise<{
+        response: {
+            pendingRequests: FriendsInvitations[];
+        };
+    }> {
         this.logger.log('getPendingFriendRequests of user_id = ', user_id);
 
-        return this.friendsService.find({
+        const pendingRequests = await this.friendsService.find({
             sender: { id: user_id },
             request_status: RequestStatus.PENDING,
         });
+
+        return {
+            response: {
+                pendingRequests,
+            },
+        };
     }
 
     /**
-     * Function to send a friend request
-     * @param sender_id
-     * @param receiver_id
-     * @returns Response with the created friend request
-     * @throws ConflictException if the sender_id and receiver_id are the same
-     * @throws NotFoundException if the receiver_id does not exist
-     * @throws InternalServerErrorException if there is an error while creating the friend request
+     * Handles the logic for sending a friend request from one user to another.
+     * Validates whether the friend request can be sent and creates a new record in the friends service if successful.
+     * Throws exceptions if the request cannot be completed due to various conflicts.
+     *
+     * @param {number} sender_id - The ID of the user sending the friend request.
+     * @param {number} receiver_id - The ID of the user receiving the friend request.
+     * @return {Promise<FriendsInvitations>} A promise that resolves with the new friend request invitation object if successful.
+     * @throws {ConflictException} If the sender attempts to send a request to themselves.
+     * @throws {ConflictException} If the sender and receiver are already friends.
+     * @throws {ConflictException} If the friend request has already been sent by the sender.
+     * @throws {ConflictException} If there is already a pending friend request from the receiver to the sender.
      */
     @UseGuards(IsExistReceiverGuard)
     @Post('send-friend-request/:receiver_id')
     async addFriend(
         @ExtractUserData('id') sender_id: number,
         @Param('receiver_id', ParseIntPipe) receiver_id: number,
-    ) {
+    ): Promise<{
+        response: {
+            friend_request: FriendsInvitations;
+        };
+    }> {
         this.logger.log(
             `add friend where sender_id =  ${sender_id}, and receiver_id = ${receiver_id}`,
         );
@@ -126,24 +178,34 @@ export class FriendsController {
             throw new ConflictException('You already received a friend request from this user');
         }
 
-        return this.friendsService.create({
+        const friend_request = await this.friendsService.create({
             sender: { id: sender_id },
             receiver: { id: receiver_id },
         } as FriendsInvitations);
+        return {
+            response: {
+                friend_request,
+            },
+        };
     }
 
     /**
-     * Function to accept a friend request
-     * @param receiver_id
-     * @param sender_id
-     * @returns Response with the updated friend request, state => accepted
-     * @throws ConflictException if the friend request is not found
+     * Accepts a pending friend request between two users; the receiver of the request
+     * confirms the request sent by the sender, updating the request status to accept.
+     *
+     * @param {number} receiver_id - The ID of the user accepting the friend request (receiver).
+     * @param {number} sender_id - The ID of the user who sent the friend request (sender).
+     * @return {Object} Returns an object with a message confirming the friend request acceptance.
      */
     @Patch('accept-friend-request/:sender_id')
     async acceptFriendRequest(
         @ExtractUserData('id') receiver_id: number,
         @Param('sender_id', ParseIntPipe) sender_id: number,
-    ) {
+    ): Promise<{
+        response: {
+            message: string;
+        };
+    }> {
         this.logger.log(
             `acceptFriendRequest where sender_id = ${sender_id} and receiver_id = ${receiver_id}`,
         );
@@ -159,23 +221,30 @@ export class FriendsController {
         );
 
         return {
-            message: 'Friend request accepted',
+            response: {
+                message: 'Friend request accepted',
+            },
         };
     }
 
     /**
-     * Function to reject a friend request
-     * @param receiver_id
-     * @param sender_id
-     * @returns Response with the updated friend request, state => rejected
-     * @throws ConflictException if the friend request is not found
+     * Rejects a friend request from the specified sender to the specified receiver.
+     * Updates the request status to "REJECTED".
+     *
+     * @param {number} receiver_id - The ID of the user who is rejecting the friend request.
+     * @param {number} sender_id - The ID of the user who sent the friend request.
+     * @return {Object} Returns an object with a message confirming the rejection of the friend request.
      */
 
     @Delete('reject-friend-request/:sender_id')
     async rejectFriendRequest(
         @ExtractUserData('id') receiver_id: number,
         @Param('sender_id', ParseIntPipe) sender_id: number,
-    ) {
+    ): Promise<{
+        response: {
+            message: string;
+        };
+    }> {
         this.logger.log(
             `rejectFriendRequest where sender_id = ${sender_id} and receiver_id = ${receiver_id}`,
         );
@@ -192,15 +261,28 @@ export class FriendsController {
         );
 
         return {
-            message: 'Friend request rejected',
+            response: {
+                message: 'Friend request rejected',
+            },
         };
     }
 
+    /**
+     * Cancels a friend request between the sender and the receiver.
+     *
+     * @param {number} sender_id - The ID of the user who sent the friend request.
+     * @param {number} receiver_id - The ID of the user to whom the friend request was sent.
+     * @return {Promise<Object>} A promise that resolves to an object containing a message confirming the cancellation of the friend request.
+     */
     @Delete('cancel-friend-request/:receiver_id')
     async cancelFriendRequest(
         @ExtractUserData('id') sender_id: number,
         @Param('receiver_id', ParseIntPipe) receiver_id: number,
-    ) {
+    ): Promise<{
+        response: {
+            message: string;
+        };
+    }> {
         this.logger.log(
             `cancelFriendRequest where sender_id = ${sender_id} and receiver_id = ${receiver_id}`,
         );
@@ -211,22 +293,29 @@ export class FriendsController {
         });
 
         return {
-            message: 'Friend request canceled',
+            response: {
+                message: 'Friend request cancelled',
+            },
         };
     }
 
     /**
-     * Function to remove a friend
-     * @param user_id user id who wants to remove the friend
-     * @param friend_id friend id to be removed
-     * @returns Response with the updated friend request, state => removed
-     * @throws InternalServerErrorException if there is an error while removing the friend
+     * Removes a friend connection between the user and the specified friend.
+     * Updates the request status to indicate the relationship has been removed.
+     *
+     * @param {number} user_id - The ID of the user initiating the friend removal.
+     * @param {number} friend_id - The ID of the friend to be removed.
+     * @return {Promise<{ message: string }>} A confirmation message indicating the friend has been removed.
      */
     @Delete('remove-friend/:friend_id')
     async removeFriend(
         @ExtractUserData('id') user_id: number,
         @Param('friend_id', ParseIntPipe) friend_id: number,
-    ) {
+    ): Promise<{
+        response: {
+            message: string;
+        };
+    }> {
         this.logger.log(`removeFriend where user_id = ${user_id} and friend_id = ${friend_id}`);
 
         await this.friendsService.findOneAndUpdate(
@@ -241,7 +330,9 @@ export class FriendsController {
         );
 
         return {
-            message: 'Friend removed',
+            response: {
+                message: 'Friend removed successfully',
+            },
         };
     }
 }
